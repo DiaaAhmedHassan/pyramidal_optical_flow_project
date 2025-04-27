83a7526f9847c30937f3ab4d0d5e2f9702500787
1MR-Joe <divosilver.144@gmail.com>
2025-04-27 23:53:14 +0300

    used frames with a smaller time gap in between

--- frame-capture.py
+++ frame-capture.py
@@ -4,25 +4,30 @@ import cv2
 cap = cv2.VideoCapture('./resources/car-traffic.mp4')
 
 
-seconds = 13
+seconds = 2
 cap.set(cv2.CAP_PROP_POS_MSEC, seconds * 1000) 
 
 # Read first frame
 ret1, frame1 = cap.read()
 
 # Go forward one second
-cap.set(cv2.CAP_PROP_POS_MSEC, (seconds+1) * 1000) 
+cap.set(cv2.CAP_PROP_POS_MSEC, (seconds+0.01) * 1000) 
 
 # Read second frame
 ret2, frame2 = cap.read()
+
+# crop the frames
+crop = 100
+cropped_frame1 = frame1[crop:frame1.shape[0]-crop, crop:]
+cropped_frame2 = frame2[crop:frame2.shape[0]-crop, crop:]
 
 # Make sure both were read successfully
 if not (ret1 and ret2):
     print("Could not read frames")
 else:
     # Convert to grayscale
-    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    gray1 = cv2.cvtColor(cropped_frame1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(cropped_frame2, cv2.COLOR_BGR2GRAY)
 
     # Show for confirmation
     while True:
@@ -33,8 +38,8 @@ else:
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    cv2.imwrite('./resources/frame1.png', gray1)
-    cv2.imwrite('./resources/frame2.png', gray2)
+    cv2.imwrite('./resources/frame3.png', gray1)
+    cv2.imwrite('./resources/frame4.png', gray2)
 
 cap.release()
 cv2.destroyAllWindows()
